ddc55a20e895455022b240a5b4fcc70949cdd917
Stathis Stamoulis <sstamoulis2010@gmail.com>
2022-12-06 11:14:08 +0200

    initial commit

---
+++ IDay.cs
@@ -0,0 +1,7 @@
+namespace adventofcode2022;
+
+interface IDay
+{
+    public object Part1();
+    public object Part2();
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace adventofcode2022;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Type[] days = new[] {
+            typeof(Day1),
+            typeof(Day2),
+            typeof(Day3),
+            typeof(Day4),
+            typeof(Day5),
+        };
+
+        foreach (var day in days)
+        {
+            var instance = Activator.CreateInstance(day);
+            if (instance is IDay dayInstance)
+            {
+                Console.WriteLine($"{day.Name} Part1: {dayInstance.Part1()}");
+                Console.WriteLine($"{day.Name} Part2: {dayInstance.Part2()}");
+            }
+        }
+    }
+}
